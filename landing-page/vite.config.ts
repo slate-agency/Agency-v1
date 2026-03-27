115ed0112b091a703ffd0098d343328bae39c2c0
slate-agency <admin@slate-automation.com>
2026-03-28 02:36:14 +0700

    U vite

--- landing-page/vite.config.ts
+++ landing-page/vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),  // ← add this
-  ],
-  base: '/',
+  plugins: [react(), tailwindcss()],
+  base: '/landing-page/',
 })
